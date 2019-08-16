22db718db8243c9c26e93cd7b3be3413fc1bfbc2
Raidro <mateus_sat@hotmail.com>
2019-08-15 21:25:14 -0300

    testando

--- src/br/senac/rn/banco/TestaBanco.java
+++ src/br/senac/rn/banco/TestaBanco.java
@@ -6,25 +6,19 @@ import br.senac.rn.banco.repository.ContaRepository;
 public class TestaBanco {
 
 
-
     public static void main(String[] args) {
 
         ContaRepository bancoDeDadosConta = new ContaRepository();
 
-        Pessoa mateus = new Pessoa("Mateus Satiro", "0000000000-1", Sexo.MASCULINO,"9999-1111");
+        Pessoa mateus = new Pessoa("Mateus Satiro", "0000000000-1", Sexo.MASCULINO, "9999-1111");
         Conta mateusConta = new ContaPoupanca();
 
         mateusConta.setAgencia("1111-1");
         mateusConta.setTitular(mateus);
 
-        bancoDeDadosConta.cadastraConta(mateus);
+        bancoDeDadosConta.cadastraConta(mateus, mateusConta);
 
-
-
-
-
-
-
-
-
+        System.out.println(mateus);
+        System.out.println(mateusConta);
+    }
 }
